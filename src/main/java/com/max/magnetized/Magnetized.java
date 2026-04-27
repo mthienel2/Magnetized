f0c54218a877a7dd61d53f9d1d0b3b855f83ae8d
Max <mthienel226@gmail.com>
2026-04-27 11:45:25 -0400

    Removed redundant registries

--- src/main/java/com/max/magnetized/Magnetized.java
+++ src/main/java/com/max/magnetized/Magnetized.java
@@ -2,26 +2,15 @@ package com.max.magnetized;
 
 import com.max.magnetized.block.ModBlocks;
 import com.max.magnetized.component.ModDataComponents;
-import com.max.magnetized.item.LightningBottleItem;
-import com.max.magnetized.item.MagnetItem;
 import com.max.magnetized.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -29,14 +18,10 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import static com.max.magnetized.block.ModBlocks.MAGNET_NULLIFIER;
 import static com.max.magnetized.item.ModItems.*;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -69,10 +54,6 @@ public class Magnetized {
     public Magnetized(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so mod items get registered
         ModItems.ITEMS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so mod blocks get registered
